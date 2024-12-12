250b10627b373b1063564d5e397f6da1c2bc6179
pranavavadivel <pranavavadivel01@example.com>
2024-12-12 15:44:18 +0530

    WIP

--- Dropdown.js
+++ Dropdown.js
@@ -1,29 +1,37 @@
+let main = document.querySelector('main')
 let showingSelectedInput = document.querySelector('#showingSelectedInput')
 let user_header = document.querySelector('.user_header')
 let dropDownIcon = document.querySelector('.dropDownIcon')
 let inputItemheader = document.querySelector('#cars')
 let showRemainigCount = document.querySelector('.remainingCount')
 let footer = document.querySelector('footer')
+let clearAllButton = document.querySelector('#clearAll')
+let doneButton = document.querySelector('#done')
+let cancelButton = document.querySelector('#cancel')
 
 let pTag = document.createElement('p')
 let img = document.createElement('img')
 let label = document.createElement('label')
 let inputTag = document.createElement('input')
 
+// utils function
+const showCssForMain = (element) => {
+    element.style.borderColor = '1px solid #ddd'
+    element.style.boxShadow = '1px 1px 10px #ddd'
+}
+
+const hideCssForMain = (element) => {
+    element.style.borderColor = ''
+    element.style.boxShadow = ''
+}
+
 // utils
-let flag = true
+let flag = false
 let selectInputLength = 0
 let iconsLink = 'Images/cancelImage.png'
 
-let usersNames = ['Ben', 'Bheem', 'Captain', 'Gwen', 'Heat_blast', 'Kevin', 'Bheem', 'Jaggu', 'Raju']
-let URLs = [ // insert images url
-    'Images/Ben.jpg',
-    'Images/Bheem.jpg',
-    'Images/captain.jpg',
-    'Images/Gwen.jpg',
-    'Images/heatBlast.jpg',
-    'Images/KevinEleven.jpg'
-]
+let usersNames = ['Ben', 'Bheem', 'Captain', 'Gwen', 'Heat_blast', 'Kevin', 'Jaggu', 'Raju']
+let URLs = []
 
 let displayMessageUser = 'Select users' // Display Messages
 
@@ -56,7 +64,6 @@ let inputs = document.querySelectorAll('input')
 function showToast(message) {
     const toast = document.querySelector('.toast');
     toast.textContent = message;
-    toast.style.backgroundColor = 'red';
     toast.classList.remove('hide');
     toast.classList.add('show');
 
@@ -66,16 +73,18 @@ function showToast(message) {
     }, 1500);
 }
 
-dropDownIcon.addEventListener('click', () => {
+user_header.addEventListener('click', () => {
     flag = !flag
     if (flag) {
         inputItemheader.style.display = 'none';
         inputItemheader.className = 'hideInputs'
         footer.style.display = 'none';
+        hideCssForMain(main)
     } else {
         inputItemheader.style.display = 'block';
         inputItemheader.className = 'showInputs'
         footer.style.display = 'flex';
+        showCssForMain(main)
     }
 })
 
@@ -100,6 +109,9 @@ inputs.forEach((val) => {
         } else {
             showingSelectedInput.textContent = `Pick users`
         }
+
+        buttonDisable()
+
     });
 });
 
@@ -126,6 +138,9 @@ function done() {
         span.className = 'selectedSpanName'
         span.textContent = val.value
         removeUserIconImg.setAttribute('src', iconsLink)
+        span.addEventListener('click', (e) => {
+            e.stopImmediatePropagation()
+        })
         span.appendChild(removeUserIconImg)
 
         showingSelectedInput.appendChild(span)
@@ -136,11 +151,12 @@ function done() {
             showRemainigCount.textContent = `+${numberOfCount}`
         }
 
-        removeUserIconImg.addEventListener('click', () => {
+        removeUserIconImg.addEventListener('click', (e) => {
+            e.stopImmediatePropagation()
             val.checked = false
             checkedValues = checkedValues.filter(value => value !== val);
             span.remove()
-            
+
             --count
             if (count <= 0) {
                 elementCheckForMessage(showingSelectedInput)
@@ -148,7 +164,6 @@ function done() {
                 let NextValueCount = checkedValues.length - 1
                 count ? showingSelectedInput.childNodes[NextValueCount].style.display = 'block' : null
                 --numberOfCount
-                console.log(numberOfCount);
                 if (numberOfCount < 1) {
                     showRemainigCount.innerHTML = ''
                 } else {
@@ -157,9 +172,26 @@ function done() {
             }
         })
     })
-
     inputItemheader.style.display = 'none';
     footer.style.display = 'none'
+    hideCssForMain(main)
+    buttonDisable()
+}
+
+function buttonDisable() {
+    if (checkedValues.length > 0) {
+        doneButton.disabled = false
+        doneButton.style.color = 'green'
+        doneButton.style.border = '1px solid rgb(0, 227, 0)'
+        clearAllButton.disabled = false
+        clearAllButton.style.color = 'darkblue'
+        clearAllButton.style.border = '1px solid darkblue'
+    }else{
+        doneButton.disabled = true
+        doneButton.removeAttribute('style')
+        clearAllButton.disabled = true
+        clearAllButton.removeAttribute('style')
+    }
 }
 
 function cancel() {
@@ -174,7 +206,7 @@ function clearAll() {
 }
 
 const elementCheckForMessage = (element) => {
-    if (checkedValues == 0) {        
+    if (checkedValues == 0) {
         element.textContent = displayMessageUser
         inputItemheader.style.display = 'block';
         footer.style.display = 'flex';
